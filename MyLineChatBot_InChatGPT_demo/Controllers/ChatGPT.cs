190b0c2e0a0c6685041b63dc8b99e6e40fda20ae
IMEI_DOM\pc080-0 <jimmy@ms5.imeifoods.com.tw>
2023-04-14 14:07:19 +0800

    加上chatGPT 3.5

---
+++ MyLineChatBot_InChatGPT_demo/Controllers/ChatGPT.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+
+namespace MyLineChatBot_InChatGPT_demo.Controllers
+{
+    public class ChatGPT
+    {
+        public static Result CallChatGPT(string msg)
+        {
+            HttpClient client = new HttpClient();
+            string uri = "https://api.openai.com/v1/chat/completions";
+
+            // Request headers.
+            client.DefaultRequestHeaders.Add(
+                "Authorization", ConfigurationManager.AppSettings["OpenAI_SecretKey"]);
+
+            var JsonString = @"
+            {
+  ""model"": ""gpt-3.5-turbo"",
+  ""messages"": [{""role"": ""user"", ""content"":""question"" }]
+}
+            ".Replace("question", msg);
+            var content = new StringContent(JsonString, Encoding.UTF8, "application/json");
+            var response = client.PostAsync(uri, content).Result;
+            var JSON = response.Content.ReadAsStringAsync().Result;
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<Result>(JSON);
+        }
+    }
+
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Choice
+    {
+        public int index { get; set; }
+        public Message message { get; set; }
+        public string finish_reason { get; set; }
+    }
+
+    public class Message
+    {
+        public string role { get; set; }
+        public string content { get; set; }
+    }
+
+    public class Result
+    {
+        public string id { get; set; }
+        public string @object { get; set; }
+        public int created { get; set; }
+        public List<Choice> choices { get; set; }
+        public Usage usage { get; set; }
+    }
+
+    public class Usage
+    {
+        public int prompt_tokens { get; set; }
+        public int completion_tokens { get; set; }
+        public int total_tokens { get; set; }
+    }
+}
--- MyLineChatBot_InChatGPT_demo/Controllers/SimpleWebHookController.cs
+++ MyLineChatBot_InChatGPT_demo/Controllers/SimpleWebHookController.cs
@@ -15,17 +15,29 @@ namespace MyLineChatBot_InChatGPT_demo.Controllers
         [HttpPost]
         public IHttpActionResult POST()
         {
-            var MyUserId = ConfigurationManager.AppSettings["MyUserId"];
+            var MyUserId = ConfigurationManager.AppSettings["Line_MyUserId"];
             try
             {
-                
-                //設定ChannelAccessToken(或抓取Web.Config)
-                this.ChannelAccessToken = ConfigurationManager.AppSettings["ChannelAccessToken"] ;
+                if (ReceivedMessage.events == null || ReceivedMessage.events.Count() <= 0 ||
+                    ReceivedMessage.events.FirstOrDefault().replyToken == "00000000000000000000000000000000") 
+                    return Ok();
+
+                //設定Line ChannelAccessToken(或抓取Web.Config)
+                this.ChannelAccessToken = ConfigurationManager.AppSettings["Line_ChannelAccessToken"] ;
                 //取得Line Event
                 var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
                 //回覆訊息
-                this.ReplyMessage(
-                    LineEvent.replyToken, "你說了:" + LineEvent.message.text);
+                var responseMsg = "";
+                if (LineEvent.message.text[0] == '!')
+                {
+                    var GptResult = ChatGPT.CallChatGPT(LineEvent.message.text).choices[0].message.content;
+                    responseMsg = $"{GptResult}";
+                }
+                else
+                {
+                    responseMsg = "你說了:" + LineEvent.message.text;                    
+                }
+                this.ReplyMessage(LineEvent.replyToken, responseMsg);
                 //response OK
                 return Ok();
             }
