5f92e2a20e01bf507092f432fc85d47989122a83
IMEI_DOM\pc080-0 <jimmy@ms5.imeifoods.com.tw>
2023-04-19 18:04:56 +0800

    (此版本無法發佈)輸出整合更新，貼圖部分有待整理

--- MyLineChatBot_InChatGPT_demo/Controllers/SimpleWebHookController.cs
+++ MyLineChatBot_InChatGPT_demo/Controllers/SimpleWebHookController.cs
@@ -24,48 +24,68 @@ namespace SimpleLineWebHook.Controllers
                     ReceivedMessage.events.FirstOrDefault().replyToken == "00000000000000000000000000000000") return Ok();
                 //取得Line Event
                 var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
-                var responseMsg = "這是預設訊息";
+                var responseMsg = "";
 
                 //等帶下波整合
-                //switch (LineEvent.type.ToLower())
-                //{
-                //    case "message": {
-                //            switch (LineEvent.message.type){
-                //                case "text":
-                //                    break;//responseMsg = $"收到 event : {LineEvent.type} type: {LineEvent.message.type} ";
-                //                case "image":
-                //                    break;
-                //                case "video":
-                //                    break;
-                //                case "audio":
-                //                    break;
-                //                case "location":
-                //                    break;
-                //                case "sticker":
-                //                    break;
-                //                default:
-                //                    break;
-                //            }
-                //            break;
-                //    }
-                //    default: 
-                //        break;
-                //}
+                switch (LineEvent.type.ToLower())
+                {
+                    case "message":
+                        {
+                            switch (LineEvent.message.type)
+                            {
+                                case "text":
+                                    {
+                                        if (LineEvent.message.text[0] == '!')
+                                        {
+                                            var GptResult = ChatGPT.CallChatGPT(LineEvent.message.text.Substring(1, LineEvent.message.text.Length - 1)).choices[0].message.content;
+                                            responseMsg = ChineseConverter.Convert($"{GptResult}", ChineseConversionDirection.SimplifiedToTraditional); ;
+                                        }
+                                        else break;
+                                        bot.ReplyMessage(LineEvent.replyToken, responseMsg);
+                                        break;//responseMsg = $"收到 event : {LineEvent.type} type: {LineEvent.message.type} ";
+                                    }                                    
+                                case "image":
+                                    break;
+                                case "video":
+                                    break;
+                                case "audio":
+                                    break;
+                                case "location":
+                                    break;
+                                case "sticker":
+                                    {
+                                        //LineEvent.message.packageId, LineEvent.message.stickerId
+                                        bot.ReplyMessage(LineEvent.replyToken, LineEvent.message.packageId, LineEvent.message.stickerId);
+                                    }
+                                    break;
+                                default:
+                                    {
+                                        responseMsg = $"收到 event : {LineEvent.type} "; 
+                                        bot.ReplyMessage(LineEvent.replyToken, responseMsg);
+                                        break;
+                                    }
+                                    
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
 
 
                 //準備回覆訊息
-                if (LineEvent.type.ToLower() == "message" && LineEvent.message.type == "text" && LineEvent.message.text[0] == '!')
-                {
-                    var GptResult = ChatGPT.CallChatGPT(LineEvent.message.text.Substring(1, LineEvent.message.text.Length-1)).choices[0].message.content;
-                    responseMsg = ChineseConverter.Convert($"{GptResult}", ChineseConversionDirection.SimplifiedToTraditional); ;
-                }
-                else if (LineEvent.type.ToLower() == "message")
-                    responseMsg = $"收到 event : {LineEvent.type} type: {LineEvent.message.type} ";
-                else
-                    responseMsg = $"收到 event : {LineEvent.type} ";
-                //回覆訊息
-                //this.ReplyMessage(LineEvent.replyToken, responseMsg);
-                bot.ReplyMessage(LineEvent.replyToken, responseMsg);
+                //if (LineEvent.type.ToLower() == "message" && LineEvent.message.type == "text" && LineEvent.message.text[0] == '!')
+                //{
+                //    var GptResult = ChatGPT.CallChatGPT(LineEvent.message.text.Substring(1, LineEvent.message.text.Length-1)).choices[0].message.content;
+                //    responseMsg = ChineseConverter.Convert($"{GptResult}", ChineseConversionDirection.SimplifiedToTraditional); ;
+                //}
+                //else if (LineEvent.type.ToLower() == "message")
+                //    responseMsg = $"收到 event : {LineEvent.type} type: {LineEvent.message.type} ";
+                //else
+                //    responseMsg = $"收到 event : {LineEvent.type} ";
+                ////回覆訊息
+                ////this.ReplyMessage(LineEvent.replyToken, responseMsg);
+                //bot.ReplyMessage(LineEvent.replyToken, responseMsg);
 
                 return Ok();
 
@@ -74,7 +94,9 @@ namespace SimpleLineWebHook.Controllers
             catch (Exception ex)
             {
                 //回覆訊息
+                //bot.ReplyMessage(LineEvent.replyToken, responseMsg);
                 bot.PushMessage(ConfigurationManager.AppSettings["Line_MyUserId"], "發生錯誤:\n" + ex.Message);
+                bot.PushMessage(ConfigurationManager.AppSettings["Line_MyUserId"], "發生錯誤:\n" + ex.StackTrace);
 
                 //response OK
                 return Ok();
---
+++ MyLineChatBot_InChatGPT_demo/Models/LineStickerDefinitions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLineChatBot_InChatGPT_demo.Models
+{
+    public class LineStickerDefinitions
+    {
+        
+        public List<LineSticker> lineStickers { 
+            
+        
+        }
+    }
+    public class LineSticker
+    {
+        public int packageId { get; set; }
+        public List<int> stickerId { get; set; }
+
+        public LineSticker(int pid, int sid) {
+            //packageId = pid;
+            if (!stickerId.Contains(sid))
+            {
+                packageId = pid;
+                stickerId.Add(sid);
+            }                        
+        }
+    }
+
+}
